d75b8fe3fbe252205df8be6210247f88d776470d
alligatormonday <joey@joeyjepson.com>
2020-12-20 15:28:23 -0700

    Redeploy to Heroku

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Uses local host OR connects to Heroku
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
